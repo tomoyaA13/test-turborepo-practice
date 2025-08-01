b92f248196e900f64584da0ff302200ed7d15e30
tomoya <alligatorfree12@gmail.com>
2025-08-01 13:08:50 +0900

    https://turborepo.com/docs/reference/package-configurations を参考にして、apps/web と apps/server のそれぞれの build script を追加

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { hc } from "hono/client";
 
 const app = new Hono();
 
@@ -7,3 +8,11 @@ const route = app.get("/hello", (c) => c.text("Hello Cloudflare Workers!"));
 export default app;
 
 export type AppType = typeof route;
+
+// Hono クライアントを生成する関数をエクスポート
+type ClientType = typeof hc<AppType>;
+export const createClient = (
+  ...args: Parameters<ClientType>
+): ReturnType<ClientType> => {
+  return hc<AppType>(...args);
+};
