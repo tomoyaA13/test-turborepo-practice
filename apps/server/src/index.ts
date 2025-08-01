688ca587f772f892d7881f27a44301d5603d1f5b
tomoya <alligatorfree12@gmail.com>
2025-08-01 12:09:00 +0900

    apps/web で hono rpc client が使用できるように変更

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello Cloudflare Workers!"));
+const route = app.get("/hello", (c) => c.text("Hello Cloudflare Workers!"));
 
 export default app;
+
+export type AppType = typeof route;
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,23 @@
-import { Button } from "@workspace/ui/components/button"
+import { Button } from "@workspace/ui/components/button";
+import { hc } from "hono/client";
+import type { AppType } from "@apps/server";
 
-export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+export default async function Page() {
+  const client = hc<AppType>("http://localhost:8787/");
+  const res = await client.hello.$get();
+
+  console.log(res);
+
+  if (res.ok) {
+    const textData = await res.text();
+    console.log(textData);
+    return (
+      <div className="flex items-center justify-center min-h-svh">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-2xl font-bold">{textData}</h1>
+          <Button size="sm">Button</Button>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
