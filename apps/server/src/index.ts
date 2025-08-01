a64a2c81efbb570cd703fc0f78a3835f4c199cba
tomoya <alligatorfree12@gmail.com>
2025-08-01 15:42:20 +0900

    Compiled Packages のドキュメント (https://turborepo.com/docs/core-concepts/internal-packages#compiled-packages) を参考にして、コードを変更

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -8,11 +8,3 @@ const route = app.get("/hello", (c) => c.text("Hello Cloudflare Workers!"));
 export default app;
 
 export type AppType = typeof route;
-
-// Hono クライアントを生成する関数をエクスポート
-type ClientType = typeof hc<AppType>;
-export const createClient = (
-  ...args: Parameters<ClientType>
-): ReturnType<ClientType> => {
-  return hc<AppType>(...args);
-};
