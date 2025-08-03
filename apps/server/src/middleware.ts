0b75a49cf71ea40dfc820d02006b5cba2b877e64
tomoya <alligatorfree12@gmail.com>
2025-08-03 09:31:11 +0900

    下記の URL を参考にして、middleware.ts file を作成 https://supabase.com/docs/guides/auth/server-side/creating-a-client?queryGroups=environment&environment=server-client&queryGroups=package-manager&package-manager=pnpm&queryGroups=framework&framework=hono

---
+++ apps/server/src/middleware.ts
@@ -0,0 +1,62 @@
+import { createServerClient, parseCookieHeader } from "@supabase/ssr";
+import { SupabaseClient } from "@supabase/supabase-js";
+import type { Context, MiddlewareHandler } from "hono";
+import { env } from "hono/adapter";
+import { setCookie } from "hono/cookie";
+
+declare module "hono" {
+  interface ContextVariableMap {
+    supabase: SupabaseClient;
+  }
+}
+
+export const getSupabase = (c: Context) => {
+  return c.get("supabase");
+};
+
+type SupabaseEnv = {
+  SUPABASE_URL: string;
+  SUPABASE_ANON_KEY: string;
+};
+
+// https://supabase.com/docs/guides/auth/server-side/creating-a-client?queryGroups=environment&environment=server-client&queryGroups=package-manager&package-manager=pnpm&queryGroups=framework&framework=hono
+export const supabaseMiddleware = (): MiddlewareHandler => {
+  return async (c, next) => {
+    const supabaseEnv = env<SupabaseEnv>(c);
+    const supabaseUrl = supabaseEnv.SUPABASE_URL;
+    const supabaseAnonKey = supabaseEnv.SUPABASE_ANON_KEY;
+
+    if (!supabaseUrl) {
+      throw new Error("SUPABASE_URL missing!");
+    }
+
+    if (!supabaseAnonKey) {
+      throw new Error("SUPABASE_ANON_KEY missing!");
+    }
+
+    const supabase = createServerClient(supabaseUrl, supabaseAnonKey, {
+      cookies: {
+        getAll() {
+          const cookieHeader = c.req.header("Cookie") ?? "";
+          const parsed = parseCookieHeader(cookieHeader);
+
+          // valueがundefinedの場合は空文字列に変換
+          return parsed.map(({ name, value }) => ({
+            name,
+            value: value ?? "",
+          }));
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            // 型の不一致は型アサーションで解決
+            setCookie(c, name, value, options as any);
+          });
+        },
+      },
+    });
+
+    c.set("supabase", supabase);
+
+    await next();
+  };
+};
