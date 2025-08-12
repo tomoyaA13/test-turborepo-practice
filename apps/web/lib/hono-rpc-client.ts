c35030414bf6b97d1e46b88d5c77a7ae918b5d08
tomoya <alligatorfree12@gmail.com>
2025-08-12 09:02:00 +0900

    API URL を環境変数で扱うように変更

--- apps/web/lib/hono-rpc-client.ts
+++ apps/web/lib/hono-rpc-client.ts
@@ -1,6 +1,10 @@
 import { hc } from "hono/client";
 import { AppType } from "@apps/server";
 
+if (!process.env.NEXT_PUBLIC_API_URL) {
+  console.log("process.env.NEXT_PUBLIC_API_URL が設定されていません");
+}
+
 console.log("honoClient が作成されました！");
 
-export const honoClient = hc<AppType>("http://localhost:8787/");
+export const honoClient = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!);
